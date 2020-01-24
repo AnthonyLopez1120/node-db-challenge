4fbe1576c006e0c79392ad24537f74840a8811d7
Anthony Lopez <anthonybeconscious@gmail.com>
2020-01-24 12:36:15 -0600

    data seeded successfully

--- data/seeds/001-task.js
+++ data/seeds/001-task.js
@@ -8,9 +8,9 @@ exports.seed = function(knex) {
         {id: 1, project_id: 1, description: 'rowValue1', notes: "stuff"},
         {id: 2, project_id: 1, description: 'rowValue2', notes: "stuff"},
         {id: 3, project_id: 2, description: 'rowValue3', notes: "stuff"},
-        {id: 3, project_id: 2, description: 'rowValue3', notes: "stuff"},
-        {id: 3, project_id: 3, description: 'rowValue3', notes: "stuff"},
-        {id: 3, project_id: 3, description: 'rowValue3', notes: "stuff"},
+        {id: 4, project_id: 2, description: 'rowValue3', notes: "stuff"},
+        {id: 5, project_id: 3, description: 'rowValue3', notes: "stuff"},
+        {id: 6, project_id: 3, description: 'rowValue3', notes: "stuff"},
       ]);
     });
 };
